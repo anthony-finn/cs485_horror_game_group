2de2320a0948337f62951d012443a571d396bda7
Anthony <>
2023-02-10 02:01:04 +0000

    Create player class, serialize and deserialize it

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -1,7 +1,9 @@
 import yaml
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-
+from player.player import player
+import pickle
+import os
 yml_configs = {}
 with open('config.yml', 'r') as yml_file:
     yml_configs = yaml.safe_load(yml_file)
@@ -9,11 +11,18 @@ with open('config.yml', 'r') as yml_file:
 
 
 def handle_request():
-    print(request.form) #['Body'])
+    phone_number = request.form['From']
+    playerObj = None
+    if (os.path.exists('players/' + phone_number = '.pk')):
+        file = open('players/' + phone_number + '.pk', 'r')
+        playerObj = pickle.load(file)
+    else:
+        playerObj = player(phone_number)
 
     message = g.sms_client.messages.create(
                      body=request.form['Body'],
                      from_=yml_configs['twillio']['phone_number'],
                      to=request.form['From'])
-
+    
+    pickle.dump(
     return json_response( status = "ok" )
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+        self.minerals = []
+
