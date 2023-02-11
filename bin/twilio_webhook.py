1c70944cab77cf597eda5bc8816589e4190b7019
Anthony Finn <30157407+anthony-finn@users.noreply.github.com>
2023-02-11 10:48:48 -0800

    Added bin, classes

---
+++ bin/twilio_webhook.py
@@ -0,0 +1,32 @@
+import yaml
+from flask import request, g
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from classes.game_state import GameState
+import pickle
+import os
+
+with open('config.yml', 'r') as yml_file:
+    yml_configs = yaml.safe_load(yml_file)
+
+def handle_request():
+    phone_number = request.form['From']
+    path = 'players/' + phone_number + '.pk'
+
+    # Load the game state, or create new if needed
+    if os.path.exists(path):
+        with open(path, 'rb') as file:
+            game = pickle.load(file)
+    else:
+        game = GameState(phone_number)
+
+    out_msg = game.run(request.form['Body'])
+
+    g.sms_client.messages.create(
+        body=out_msg,
+        from_=yml_configs['twillio']['phone_number'],
+        to=request.form['From'])
+
+    with open(path, 'wb') as file:
+        pickle.dump(game, file)
+
+    return json_response(status = "ok")
